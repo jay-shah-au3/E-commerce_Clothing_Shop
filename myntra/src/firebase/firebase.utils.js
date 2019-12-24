30389bcbc1d11a3b71b541d1edf82c747e66fe88
jay-shah-au3 <shhahjay@gmail.com>
2019-12-24 21:47:35 +0530

    Stripe Payment Integration

---
+++ myntra/src/firebase/firebase.utils.js
@@ -0,0 +1,51 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCOFIzVUsJ3tlEGITCpzju2GztRgvTXfwI",
+    authDomain: "myntra-db.firebaseapp.com",
+    databaseURL: "https://myntra-db.firebaseio.com",
+    projectId: "myntra-db",
+    storageBucket: "myntra-db.appspot.com",
+    messagingSenderId: "742257728100",
+    appId: "1:742257728100:web:ed4defa22bd15a1f035d71",
+    measurementId: "G-MFJ0Q4RXWC"
+};
+
+export const createUserProfileDocument = async (userAuth, additonalData) => {
+    if(!userAuth)
+        return;
+    const userRef = firestore.doc(`/users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+
+    if(!snapShot.exists){
+        const {displayName, email} = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additonalData
+            })
+        }
+        catch(error){
+            console.log("error creating user", error.message);
+        }
+    }
+    return userRef;
+} 
+
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({'prompt':'select_account'});
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
---
+++ myntra/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {BrowserRouter as Router} from 'react-router-dom';
+import {PersistGate} from "redux-persist/integration/react";
+import {store, persistor} from './redux/store';
+import App from './App';
+import './index.css';
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Router>
+            <PersistGate persistor={persistor}>
+                <App/>
+            </PersistGate>
+        </Router>
+    </Provider>, 
+    document.getElementById('root'));
+
